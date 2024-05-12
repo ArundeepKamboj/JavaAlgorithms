84d329b7e96ccb8f925682044c37ab619b561450
ArundeepKamboj <arundeep.kamboj@gmail.com>
2024-05-12 17:04:47 +0530

    Left rotate array

--- src/main/java/org/array/Main.java
+++ src/main/java/org/array/Main.java
@@ -41,6 +41,9 @@ public class Main {
 
         Main.singleDimensionArray();
         Main.twoDimensionArray();
+
+        boolean res = RotateMatrix.rotateMatrix(two_d_array_1);
+
     }
 
     static void singleDimensionArray(){
