5f058b0275828add143cbe461fc247ed3f22e613
ArundeepKamboj <arundeep.kamboj@gmail.com>
2024-05-26 14:33:38 +0530

    Insertion in circular Singly linked list.

--- src/main/java/org/linkedlist/CircularSinglyLinkedList.java
+++ src/main/java/org/linkedlist/CircularSinglyLinkedList.java
@@ -15,5 +15,32 @@ public class CircularSinglyLinkedList {
         return head;
     }
 
+    public void insert(int nodeValue, int location){
+        if(head==null){
+            createCSLL(nodeValue);
+            return;
+        }
+        Node node = new Node();
+        node.value = nodeValue;
+        if(location ==0 ){
+            node.next = head;
+            tail.next = node;
+            head = node;
+        } else if(location>=size){
+            tail.next = node;
+            tail = node;
+            tail.next = head;
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while(index<location-1){
+                tempNode = tempNode.next;
+                index++;
+            }
+            node.next = tempNode.next;
+            tempNode.next = node;
+        }
+        size++;
+    }
 
 }
--- src/main/java/org/linkedlist/CircularSinglyLinkedListMain.java
+++ src/main/java/org/linkedlist/CircularSinglyLinkedListMain.java
@@ -5,6 +5,10 @@ public class CircularSinglyLinkedListMain {
         CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
 
         csll.createCSLL(5);
+        csll.insert(4,0);
+        csll.insert(1,8);
+        csll.insert(2,2);
+        csll.insert(5,3);
         System.out.println(csll.head.value);
         System.out.println(csll.head.next.value);
     }
